df932d7fb48ec45a01e28adbb8dc5441bcc96fa8
surajguvaju079 <surajguvaju.079@kathford.edu.np>
2025-02-03 22:27:46 +0545

    index page about to be completed

--- src/script/index.js
+++ src/script/index.js
@@ -4,3 +4,17 @@ document.querySelectorAll(".accordion-button").forEach((button) => {
     content.style.display = content.style.display === "flex" ? "none" : "flex";
   });
 });
+
+function handleRead() {
+  const read_more = document.querySelector("#read-more");
+  console.log("read_more nodes", read_more);
+  const button_text = document.querySelector("#button-text span");
+  console.log("button text:", button_text);
+  if (read_more.style.display === "block") {
+    read_more.style.display = "none";
+    button_text.innerHTML = "Read More";
+  } else {
+    read_more.style.display = "block";
+    button_text.innerHTML = "Read Less";
+  }
+}
